02d69b63bae1b5b0ba607eb69c7c1f9ed1fd1019
Andre T Gama <andre_gama789@hotmail.com>
2026-08-03 21:43:00 -0300

    feat: add messages in hero

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 import image from "@/assets/img/conference-speaker.jpg";
 
 export default function Hero() {
+  const t = useTranslations("home");
   return (
     <section id="hero" className="py-10">
       <div className="flex flex-col items-center gap-12 md:flex-row bg-blue-500">
         <div className="flex flex-col justify-center md:w-3/5 px-8">
           <h1 className="mb-8 text-4xl font-bold leading-tight sm:text-5xl md:text-6xl lg:text-7xl">
-            Building scalable solutions for real problems.
+            {t("hero.h1")}
           </h1>
           <div className="space-y-6">
             <p className="max-w-2xl text-lg leading-8 md:text-xl">
-              Based in Brazil, I build scalable web applications using PHP,
-              Laravel, AWS and modern web technologies.
+              {t("hero.textOne")}
             </p>
             <p className="max-w-2xl text-lg leading-8 md:text-xl">
-              Passionate about clean architecture, scalable systems and
-              continuous learning, I strive to create software that is
-              reliable, maintainable and built to last.
+              {t("hero.textTwo")}
             </p>
           </div>
         </div>
